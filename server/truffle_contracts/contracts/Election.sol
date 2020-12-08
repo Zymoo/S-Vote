836111b9854ebd9d41492a28439d1eaf65b16d8f
Tomek Zymkowski <t.zymkowski@gmail.com>
2020-12-08 12:01:30 +0100

    SmartContract finished

--- server/truffle_contracts/contracts/Election.sol
+++ server/truffle_contracts/contracts/Election.sol
@@ -8,12 +8,16 @@ contract Election {
         uint number;
     }
 
-    struct Voter {
-        string fullName;
-        string publicKey;
+    struct Result {
+        int256 voteSum;
+        int64[] individualScores;
+        string ephermalKey;
     }
 
+    Result electionResult;
     Candidate[] candidates;
+    string[] voterKeys;
+    string[] votes;
     string publicKey;
 
     function savePublicKey (string memory key) public {
@@ -21,17 +25,36 @@ contract Election {
     }
 
     function saveCandidates (string[] memory names, uint[] memory numbers) public {
-        for ( uint i = 0; i < names.length; i++){
+        for (uint i = 0; i < names.length; i++){
             candidates.push(Candidate({fullName: names[i], number: numbers[i]}));
         }
+    }
+
+    function saveResult(int256 sum, int64[] memory scores, string memory ephermal) public {
+        electionResult = Result({voteSum: sum, individualScores: scores, ephermalKey: ephermal});
+    }
+
+    function saveVoterKey(string memory newKey) public {
+        voterKeys.push(newKey);
+    }
+
+    function saveVote(string memory newVote) public {
+        votes.push(newVote);
     }
 
     function getPublicKey() public view returns (string memory) {
         return publicKey;
     }
 
-    // function getCandidates() public view returns (Candidate[] memory) {
-    //     return candidates;
-    // }
+    function getCandidates() public view returns (Candidate[] memory) {
+        return candidates;
+    }
 
+    function getVotes() public view returns (string[] memory) {
+        return votes;
+    }
+
+    function getResult() public view returns (Result memory){
+        return electionResult;
+    }
 }
--- server/utilities/trufflechain.js
+++ server/utilities/trufflechain.js
@@ -1,12 +1,13 @@
-/* eslint-disable require-jsdoc */
 const Web3 = require('web3');
 const provider = new Web3.providers.HttpProvider('http://localhost:7545');
 const contract = require('truffle-contract');
-// eslint-disable-next-line max-len
+const contractPath = '../truffle_contracts/build/contracts/Election.json';
+const contractJson = require(contractPath);
+const serverAddress = '0x542E86396c4aBe1394F7B7d5Cec628d5878fC8cD';
+const contractAddress = '0x1525b42Cd6Bb18a8bf93De9Dc0d2a20a1B2Be43D';
+// IMPORTANT - after each new migration this adress will change!
 
-const contractJson = require(
-    '../truffle_contracts/build/contracts/Election.json');
-const contractAddress = '0x7555e10036C2feA9dF695624A2085941B7f8f3f0';
+
 const ElectionContract = contract({
   abi: contractJson.abi,
   unlinked_binary: contractJson.bytecode,
@@ -16,10 +17,9 @@ ElectionContract.setProvider(provider);
 
 exports.saveElectionKey = async function() {
   const instance = await ElectionContract.at(contractAddress);
-  const fromAddress = '0x6aC3F1604BDF80Af4Bde610617F90b471Cf46D4e';
   try {
     const result = await instance
-        .savePublicKey('TestKey', {from: fromAddress});
+        .savePublicKey('TestKey', {from: serverAddress});
     console.log(result);
   } catch (err) {
     console.log(err);
@@ -29,12 +29,11 @@ exports.saveElectionKey = async function() {
 
 exports.saveCandidates = async function() {
   const instance = await ElectionContract.at(contractAddress);
-  const fromAddress = '0x6aC3F1604BDF80Af4Bde610617F90b471Cf46D4e';
   const nameParams = ['Jan Kowalski', 'Borys Nowak'];
   const numParams = [1, 1000];
   try {
     const result = await instance
-        .saveCandidates(nameParams, numParams, {from: fromAddress});
+        .saveCandidates(nameParams, numParams, {from: serverAddress});
     console.log(result);
   } catch (err) {
     console.log(err);
@@ -44,8 +43,8 @@ exports.saveCandidates = async function() {
 
 // Showcase for interacting with contract on blockchain
 exports.testerIncrement = async function() {
-  // eslint-disable-next-line max-len
-  const contractJson = require('../truffle_contracts/build/contracts/Tester.json');
+  const contractJson = require(
+      '../truffle_contracts/build/contracts/Tester.json');
   const contractAddress = '0xa4570D3B5C7aA483DD6f977E7134a4a8bc308cD1';
   const MyContract = contract({
     abi: contractJson.abi,
