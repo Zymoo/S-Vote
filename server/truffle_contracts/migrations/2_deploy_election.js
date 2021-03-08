5832ab8584401e978e632b465e4e82a6e570659a
Tomek Zymkowski <t.zymkowski@gmail.com>
2021-03-08 15:16:41 +0100

    Migration fix

--- server/truffle_contracts/migrations/2_deploy_election.js
+++ server/truffle_contracts/migrations/2_deploy_election.js
@@ -1,5 +1,10 @@
-const Election = artifacts.require('./Election.sol');
+const ElectionToken = artifacts.require('ElectionToken');
+const CrowdSale = artifacts.require('CrowdSale');
+const Election = artifacts.require('Election');
 
-module.exports = function(deployer) {
-  deployer.deploy(Election);
+module.exports = async function(deployer) {
+  const tokenPrice = 1000000000000000;
+  await deployer.deploy(ElectionToken, 1000000);
+  await deployer.deploy(CrowdSale, ElectionToken.address, tokenPrice);
+  await deployer.deploy(Election, ElectionToken.address, CrowdSale.address);
 };
